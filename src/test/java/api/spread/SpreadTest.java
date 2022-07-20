8c16142590bdaf973cb667d67d04d1e7423ce4da
bjarzynski <91565134+bjarzynski@users.noreply.github.com>
2022-07-20 22:17:38 +0200

    feat: add new test to check pair of Spread.
    Added test which checks if pair given in the subscription is the as in the spread message.

--- src/test/java/api/spread/SpreadTest.java
+++ src/test/java/api/spread/SpreadTest.java
@@ -3,6 +3,7 @@ package api.spread;
 import api.*;
 import com.google.gson.Gson;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.JsonUtils;
 
@@ -10,6 +11,13 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class SpreadTest {
+  @DataProvider(name = "test-data")
+  public Object[][] dataProviderApi() {
+    return new Object[][]{
+            {"XBT/USD"}, {"ETH/USD"}, {"XDG/USD"}
+    };
+  }
+
   @Test
   public void checkBidAsk() throws URISyntaxException {
     WebSocketKrakenClient krakenClient = new WebSocketKrakenClient(new URI("wss://ws.kraken.com"));
@@ -23,5 +31,20 @@ public class SpreadTest {
     krakenClient.awaitForMessage(10);
     SpreadMessage spreadMessage = new SpreadMessage(krakenClient.getLastMessage());
     Assert.assertTrue(spreadMessage.getAsk() > spreadMessage.getBid());
+  }
+
+  @Test(dataProvider = "test-data")
+  public void checkPair(String pair) throws URISyntaxException {
+    WebSocketKrakenClient krakenClient = new WebSocketKrakenClient(new URI("wss://ws.kraken.com"));
+    krakenClient.connect();
+    krakenClient.awaitForMessage(10);
+    SubscriptionMessage subscriptionMessage = new SubscriptionMessage("subscribe", new String[]{pair}, new Subscription("spread"));
+    krakenClient.prepareMessageAwaiting();
+    krakenClient.send(JsonUtils.getJsonString(subscriptionMessage));
+    krakenClient.awaitForMessage(10);
+    krakenClient.prepareMessageAwaiting();
+    krakenClient.awaitForMessage(10);
+    SpreadMessage spreadMessage = new SpreadMessage(krakenClient.getLastMessage());
+    Assert.assertEquals(spreadMessage.getPair(), pair);
   }
 }
